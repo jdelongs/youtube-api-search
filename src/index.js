6a04af52ff697badd86b5b915836e26512256bf1
jdelong <jdelong15@hotmail.ca>
2018-10-29 15:53:16 -0700

    changed index from a function component to a class component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //import statements
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; 
 import YTSearch from 'youtube-api-search'; 
 
@@ -7,17 +7,26 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; 
 import API_KEY from './components/api_key'; 
  
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data); 
-}); 
 
 //create a new component that generates html
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    ); 
+class App extends Component {
+    constructor(props) {
+        super(props); 
+
+        this.state = { videos: [] }; 
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); 
+        }); 
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        ); 
+    }
 }
 
 
