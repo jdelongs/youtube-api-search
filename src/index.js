f07f081f971b3ac6e2f71b948d73df36e092c905
jdelong <jdelong15@hotmail.ca>
2018-11-14 12:51:57 -0800

    added clickable functionality to video list

--- src/index.js
+++ src/index.js
@@ -16,11 +16,17 @@ class App extends Component {
     constructor(props) {
         super(props); 
 
-        this.state = { videos: [] }; 
+        this.state = { 
+            videos: [], 
+            selectedVideo: null 
+        }; 
 
         //youtube api search method
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos }); 
+            this.setState({ 
+                videos: videos, 
+                selectedVideo: videos[0]
+            }); 
         }); 
     }
 
@@ -28,8 +34,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                    videos={this.state.videos} />
             </div>
         ); 
     }
